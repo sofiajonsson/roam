bfa13ccca64661fb904810e7d610107daf5d667a
Sofia <jonssonsofia93@gmail.com>
2019-04-16 09:44:08 -0700

    created comments and users controllers and added a route to the config file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,30 @@
 class CommentsController < ApplicationController
+	def new
+		@comment = Comment.new
+	end
+
+	def create
+		Comment.create(comment_params)
+			redirect_to posts_path
+
+	def update
+		@comment = Comment.update(comment_params)
+			if @comment.save
+				redirect_to @comment
+			else
+				render :edit
+			end
+		end
+
+		def show
+			@comment = Comment.find(params[:id])
+		end
+
+		private
+
+		def comment_params
+			params.require(:comment).permit!
+			#same thing, will add correct params later
+		end
+
 end
