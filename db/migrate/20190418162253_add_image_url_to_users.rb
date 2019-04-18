9874acb2b4096b3235116b18b883ccc7e4f3af36
Sofia <jonssonsofia93@gmail.com>
2019-04-18 10:50:35 -0700

    Added images to user model, seeded, worked on user show page

---
+++ db/migrate/20190418162253_add_image_url_to_users.rb
@@ -0,0 +1,5 @@
+class AddImageUrlToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :photo_url, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_212541) do
+ActiveRecord::Schema.define(version: 2019_04_18_162253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2019_04_16_212541) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "photo_url"
   end
 
 end
